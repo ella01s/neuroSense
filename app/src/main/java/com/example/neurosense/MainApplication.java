5efdeaffcfc0bd2d9ccc247852f50ff217fc10ee
ella01s <85433513+ella01s@users.noreply.github.com>
2023-10-04 21:42:36 +0100

    Delete app/src/main/java/com/example/neurosense/MainApplication.java

--- app/src/main/java/com/example/neurosense/MainApplication.java
+++
@@ -1,8 +0,0 @@
-package com.example.neurosense;
-import com.bfr.buddysdk.BuddyApplication;
-
-public class MainApplication extends BuddyApplication {
-    @Override
-    public void onCreate() { super.onCreate();
-    } //end onCreate
-}
